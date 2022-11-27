76db1b999252d157c91469fc748fe4ad715708b8
A Ut Hong <33076940+AlexH2021@users.noreply.github.com>
2022-11-27 15:00:47 -0800

    Update login.js

--- result_login/nodelogin/login.js
+++ result_login/nodelogin/login.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const result_url =  process.env.OUTPUT_LB_SERVICE_HOST
+const result_url =  process.env.output_host
 
 function return_True() {
     return true;
